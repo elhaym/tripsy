81eb8a7156fbef6366778196f17cbc5854b64839
Elhaym <elhaym@users.noreply.github.com>
2019-03-14 00:37:55 +0100

    Updates

---
+++ precache-manifest.651a822d5430a97d7631c74cbf0a48a3.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf78e54a21d4efd37810",
+    "url": "/tripsy/static/css/main.f1b41164.chunk.css"
+  },
+  {
+    "revision": "bf78e54a21d4efd37810",
+    "url": "/tripsy/static/js/main.bf78e54a.chunk.js"
+  },
+  {
+    "revision": "89e842944d96ffb00154",
+    "url": "/tripsy/static/js/runtime~main.89e84294.js"
+  },
+  {
+    "revision": "9d62d531a54f481f1c65",
+    "url": "/tripsy/static/css/2.354082c1.chunk.css"
+  },
+  {
+    "revision": "9d62d531a54f481f1c65",
+    "url": "/tripsy/static/js/2.9d62d531.chunk.js"
+  },
+  {
+    "revision": "5f25f98ad449e14f35dbe987e0d090e8",
+    "url": "/tripsy/static/media/manrope-extrabold.5f25f98a.woff"
+  },
+  {
+    "revision": "7af88092dbd37f08241da9f416dee798",
+    "url": "/tripsy/static/media/manrope-extrabold.7af88092.woff2"
+  },
+  {
+    "revision": "a00289fe0098f0dc10fcc3ece8073bb6",
+    "url": "/tripsy/static/media/manrope-semibold.a00289fe.woff2"
+  },
+  {
+    "revision": "a6fa3bd125ff6df3f77ce6c4203e4282",
+    "url": "/tripsy/static/media/manrope-bold.a6fa3bd1.woff2"
+  },
+  {
+    "revision": "8325e7ea6f0adcae48d1dbc6e45d1951",
+    "url": "/tripsy/static/media/manrope-semibold.8325e7ea.woff"
+  },
+  {
+    "revision": "441244cf44090a0ff88abc33fbbca97d",
+    "url": "/tripsy/static/media/manrope-medium.441244cf.woff2"
+  },
+  {
+    "revision": "3cea6e29c4001d672667a93775cb061e",
+    "url": "/tripsy/static/media/manrope-regular.3cea6e29.woff"
+  },
+  {
+    "revision": "3c100c613625f06a5e00ddbba18b7f61",
+    "url": "/tripsy/static/media/manrope-regular.3c100c61.woff2"
+  },
+  {
+    "revision": "37c00b97e26ac9fe903a6ccc9ea48644",
+    "url": "/tripsy/static/media/manrope-light.37c00b97.woff2"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/tripsy/static/media/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "6c9f93a2edff065d81710586bf74fbe3",
+    "url": "/tripsy/static/media/manrope-thin.6c9f93a2.woff2"
+  },
+  {
+    "revision": "484b6c5b3a6f889b62b1985d44467096",
+    "url": "/tripsy/static/media/manrope-thin.484b6c5b.woff"
+  },
+  {
+    "revision": "c246dc6aecd6b7007b935466d04e3082",
+    "url": "/tripsy/static/media/manrope-bold.c246dc6a.woff"
+  },
+  {
+    "revision": "16a2e274512a3e5f752ee83b66ed0d9d",
+    "url": "/tripsy/static/media/manrope-medium.16a2e274.woff"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/tripsy/static/media/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "860d372f9b96d1b53a5b82a6cec33878",
+    "url": "/tripsy/static/media/manrope-light.860d372f.woff"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/tripsy/static/media/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "9447e9428e0e17849179799e1a391cc0",
+    "url": "/tripsy/static/media/bg_banner_drive.9447e942.jpg"
+  },
+  {
+    "revision": "6b7897c9d28b17fce51b4b20052bf93f",
+    "url": "/tripsy/static/media/manrope-extrabold.6b7897c9.otf"
+  },
+  {
+    "revision": "cb519ea75f9f92a1ed78efdf2f4fdfef",
+    "url": "/tripsy/static/media/manrope-bold.cb519ea7.otf"
+  },
+  {
+    "revision": "ae7c5af0c35d94dbcd1c599fc7b400b7",
+    "url": "/tripsy/static/media/manrope-semibold.ae7c5af0.otf"
+  },
+  {
+    "revision": "31973fa52e481ba490f242d2ab896fd2",
+    "url": "/tripsy/static/media/manrope-medium.31973fa5.otf"
+  },
+  {
+    "revision": "b0a4a6ccf7d97d473f4590541a20f950",
+    "url": "/tripsy/static/media/manrope-regular.b0a4a6cc.otf"
+  },
+  {
+    "revision": "85b96796bdaef0c326c1bbb18f0222ea",
+    "url": "/tripsy/static/media/manrope-light.85b96796.otf"
+  },
+  {
+    "revision": "ab2fd20ee1acaabb676ee6fd2432437b",
+    "url": "/tripsy/static/media/manrope-thin.ab2fd20e.otf"
+  },
+  {
+    "revision": "684054b30da2d63ba68f62a3d51be59c",
+    "url": "/tripsy/static/media/bg_map_800x800.684054b3.jpg"
+  },
+  {
+    "revision": "55242ea5c378da626cab5b4aaf9c2255",
+    "url": "/tripsy/static/media/MaterialIcons-Regular.55242ea5.ttf"
+  },
+  {
+    "revision": "df679513a148a2d369b77dd9bef46e60",
+    "url": "/tripsy/static/media/bg_lgn_1200x800.df679513.jpg"
+  },
+  {
+    "revision": "2512df3c91857a4b0e716b98372e5632",
+    "url": "/tripsy/static/media/bb_street_bg_1200x800.2512df3c.jpg"
+  },
+  {
+    "revision": "bf765f1f671c7fe89393e5b0b1fb854d",
+    "url": "/tripsy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tripsy/precache-manifest.1a8669801abeb74586b93b6de51b0f03.js"
+  "/tripsy/precache-manifest.651a822d5430a97d7631c74cbf0a48a3.js"
 );
 
 workbox.clientsClaim();
